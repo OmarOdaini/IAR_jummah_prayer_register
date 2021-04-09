0364ced803bf3d89919a19f1e9c3b49082c9a745
Omar Odaini <alodainiomar@gmail.com>
2021-04-08 21:12:45 -0400

    new update

--- register.py
+++ register.py
@@ -11,6 +11,7 @@ def main(Registrants, Shifts):
         try:
             # new driver 
             driver = GetDriver()
+            
             # get to IAR Jummah page
             driver.get("https://raleighmasjid.org/jumaa")
             assert "The Islamic Association of Raleigh - Jumuah Registration" in driver.title
@@ -18,11 +19,16 @@ def main(Registrants, Shifts):
             # get to eventbrite page
             driver.get(GetLinkfromBtn("Register for Jumuah Shifts", driver))
             assert "IAR Jumu'ah Registration" in driver.title
-            ClickRegister(driver)
 
+            # Click Select A Date
+            GetLinkfromBtnByXPATH("//button[starts-with(@id, 'eventbrite-widget-modal-trigger-')]", driver).click()
+                                    
             # switch iframe (to access pop-ups)
             driver.switch_to.frame(driver.find_element_by_xpath("//iframe[starts-with(@id, 'eventbrite-widget-modal-')]"))
             sleep(2)
+
+            # Select Jummah
+            ClickCatagory("Fri, 11:00 AM - 3:15 PM EDT", driver)
 
             # Increment to specified catagory
             IncrementCatagory(Shifts[person['catagory']][person['timeslot']], driver)
@@ -44,9 +50,9 @@ def GetDriver():
     chromePath = "{}/chromedriver".format(dirname(abspath(__file__)))
     # options object
     options = Options()
-    # silent mode
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu') 
+    # # silent mode
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu') 
     return Chrome(chromePath, options=options)
 
 def IncrementCatagory(catagory, driver):
@@ -56,6 +62,13 @@ def IncrementCatagory(catagory, driver):
     # Relateive XPATH based on catagory name
     incrementXPATH = "//h3[text()='{}']/{}/option[text()='{}']".format(catagory, TitletoSelectPath, number)
     # Increment 
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, incrementXPATH))).click()
+
+def ClickCatagory(catagory, driver):
+    btnText = "Tickets"
+    # Relateive XPATH based on catagory name
+    incrementXPATH = "//div[text()='{}']/../../../div[2]/div/button[text()='{}']".format(catagory, btnText)
+    # click 
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, incrementXPATH))).click()
 
 def FillContactInfo(fname, lname, email, phone, driver):
@@ -101,10 +114,16 @@ def GetLinkfromBtn(text, driver):
         EC.presence_of_element_located((By.LINK_TEXT, text))
     ).get_attribute('href')
 
+def GetLinkfromBtnByXPATH(xpath, driver):
+    return WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, xpath))
+    )
+
 if __name__ == "__main__":
     # People to register
     Registrants = [
         # FOLLOW THE EXAMPLE BELOW FOR PESONAL INFO FORMAT (only update the right side after colon):
+        
         # {
         #     "catagory": "men or women",
         #     "timeslot": "timeslot",
@@ -112,15 +131,8 @@ if __name__ == "__main__":
         #     "lastname": "lastname",
         #     "email": "email to get tickets",
         #     "phone": "phone #"
-        # },
-        {
-            "catagory": "men",
-            "timeslot": "3:00",
-            "firstname": "test", 
-            "lastname": "test",
-            "email": "test@gmail.com",
-            "phone": "2342342343"
-        }
+        # }
+        
     ]
     # Shifts details
     Shifts = {
